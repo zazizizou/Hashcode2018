f8f2b16245dd4fcea6568ea6463a7cee497b00c0
Habib Gahbiche <habibgahbiche@gmail.com>
2018-02-28 23:31:09 +0100

    created all necessary functions. code still buggy

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -27,15 +27,25 @@ def get_tom_mush_number(content):
 
 class Slice:
 
-    def __init__(self, a, b, content):
+    def __init__(self, a, b, pizza):
         self.ax = a[0]
         self.ay = a[1]
         self.bx = b[0]
         self.by = b[1]
-        self.content = content
+        self.pizza = pizza
+        slice_content = pizza.content[a[0]:(b[0]+1), a[1]:(b[1]+1)]
+        self.content = slice_content
 
-        self.l_tom, self.l_mush = get_tom_mush_number(content)
-        self.h = content.size()
+        self.l_tom, self.l_mush = get_tom_mush_number(self.content)
+        self.h = self.content.size
+
+    def __add__(self, other):
+        ax = min(self.ax, other.ax)
+        ay = min(self.ay, other.ay)
+        bx = max(self.bx, other.bx)
+        by = max(self.by, other.by)
+        self.pizza.content = self.pizza.content[ax:(bx+1),ay:(by+1)]
+        return Slice((ax,ay), (bx,by), self.pizza)
 
 
 def pizza_read(filename):
@@ -53,21 +63,52 @@ def pizza_read(filename):
     return Pizza(R, C, L, H, content)
 
 
-def get_slice(pizza, a, b):
-    slice_content = pizza.content[a[0]:b[0]][a[1]:b[1]]
-    slice = Slice(a, b, slice_content)
-    return slice
+def are_neighbours(r1, r2):
+    if np.abs(r2.ax - r1.ax) == (r1.bx - r1.ax + 1) :
+        return True
+    elif np.abs(r2.ay - r1.ay) == (r1.by -r1.ay + 1):
+        return True
+    else:
+        return False
 
 
-def cut(pizza):
+def cut(pizza, nb_iterations=10):
+    L = pizza.L
+    H = pizza.H
+
     # get initial regions
-    regions_init = []
+    regions = []
+    regions_removed = set()
     for i, _ in enumerate(pizza.content):
         for j, _ in enumerate(pizza.content):
-            slice_content = pizza.content[i][j]
-            regions_init += [Slice((i, j), (i, j), slice_content)]
+            new_content = np.array([[  pizza.content[i,j]  ]])
+            new_pizza = Pizza(pizza.r, pizza.c, pizza.l, pizza.h, new_content)
+            regions += [
+                Slice((i, j), (i, j), new_pizza)
+            ]
+
+    for _ in range(nb_iterations):
+        for r1 in regions:
+            for r2 in regions:
+                if r1 != r2 and are_neighbours(r1, r2):
+                    merged = r1 + r2
+                    if merged.h < H and merged.l_mush >= L and merged.l_tom >= L:
+                        regions_removed.add(r1)
+                        regions_removed.add(r2)
+                        regions += [merged]
+
+    regions_final = [r for r in regions if r not in regions_removed]
+    return regions_final
 
 
+def write_result(regions_final, filename):
+    with open(filename, "w") as file:
+        file.write(len(regions_final))
+        for region in regions_final:
+            file.write((str(region.ax)+ " "+
+                        str(region.ay)+ " "+
+                        str(region.bx)+ " "+
+                        str(region.by)+ " "))
 
 
 def main():
@@ -78,18 +119,24 @@ def main():
     pizza = Pizza(3, 5, 1, 6, content)
     print(pizza.content)
 
-    pizza.read("small.in")
+    pizza = pizza_read("small.in")
     print("Content after reading")
     print(pizza.content)
     assert pizza.C == 7
     assert pizza.R == 6
 
+    myslice = Slice((0,0), (1,1), pizza)
+    print("new slice content:")
+    print(myslice.content)
+    assert myslice.h == 4
+    assert myslice.l_mush == 3
+    assert myslice.l_tom == 1
 
+    # res = Slice((0,0), (1,1), pizza) + Slice((0,0), (2,2), pizza)
+    # print(res.content)
+    regions = cut(pizza)
+    write_result(regions, "small.out")
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
